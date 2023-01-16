2b21d103f02f9788c0377c7051c2b3b434f75e66
Jesse Akorah <jesseakorah@gmail.com>
2023-01-16 16:54:22 +0100

    Load assets

---
+++ src/assets.js
@@ -0,0 +1,7 @@
+export const assets = {
+  expo: "/assets/expo.png",
+  hero1: "/assets/home_hero.png",
+  hero2: "assets/feature.png",
+  reactIcon: "/assets/react-icon.png",
+  javascriptIcon: "assets/javascript.png",
+};
